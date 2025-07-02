0ea62c8741b73c1a38d7cb6f7ab8a35c30897fa5
david-arvidsson <david.arvidsson@humly.io>
2025-07-02 21:10:52 +0200

    fix. env.port

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,9 @@ const pool = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 app.use(cors({
